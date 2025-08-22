773c7ce5031394e15dbb6acff32cf5595b67a60f
Janineeeeee12 <beautifulmaria09876@gmail.com>
2025-08-22 16:50:06 +0700

    change color

--- app/index.jsx
+++ app/index.jsx
@@ -29,7 +29,7 @@ const styles = StyleSheet.create({
   link: {
     marginVertical: 20,
     padding: 16,
-    backgroundColor: '#a46ec4ff',
+    backgroundColor: '#81552cff',
     color: 'white',
     borderRadius: 8,
   },
